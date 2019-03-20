0db8809b2e313f3a2526327ca4efc5ff3f826564
basfroman <basfroman@gmail.com>
2019-03-20 15:07:03 -0700

    chance log

--- The Office IV - Find a Meeting Room.js
+++ The Office IV - Find a Meeting Room.js
@@ -7,8 +7,4 @@ function meeting(x){
 }
 
 
-Test.describe("Example tests",_=>{
-    Test.assertEquals(meeting(['X', 'O', 'X']), 1);
-    Test.assertEquals(meeting(['O','X','X','X','X']), 0);
-    Test.assertEquals(meeting(['X','X','X','X','X']), 'None available!');
-});
+console.log(meeting(['X', 'O', 'X']));
