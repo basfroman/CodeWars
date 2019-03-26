cbb1d85370f8e62baf2b6b53279cbf0ddd97a6b5
basfroman <basfroman@gmail.com>
2019-03-26 14:41:12 -0700

    Next bigger number with the same digits

--- _Next bigger number with the same digits.js
+++ _Next bigger number with the same digits.js
@@ -2,50 +2,50 @@
 kata https://www.codewars.com/kata/next-bigger-number-with-the-same-digits/train/javascript
  */
 
-function nextBigger(n){
+function nextBigger(n) {
     //console.log(n);
     let out = '';
     let new_out = '';
     let new_n = String(n);
-    for (let i = new_n.length - 1; i>=0; i--){
-        if (new_n[i] > new_n[i-1]){
-            let find_sec = new_n.substring(i-1);
+    for (let i = new_n.length - 1; i >= 0; i--) {
+        if (new_n[i] > new_n[i - 1]) {
+            let find_sec = new_n.substring(i - 1);
             //console.log(+new_n.substring(i-1), nextBigger2(+find_sec));
-            out = nextBigger2(+find_sec)[0] + new_n.substring(1, nextBigger2(find_sec)[1]) + new_n[0] + find_sec.substring(nextBigger2(+find_sec)[1]+1);
-            new_out = getReplaseMaxMin (out);
+            out = nextBigger2(+find_sec)[0] + new_n.substring(1, nextBigger2(find_sec)[1]) + new_n[0] + find_sec.substring(nextBigger2(+find_sec)[1] + 1);
+            new_out = getReplaseMaxMin(out);
             break
         }
     }
 
-    return new_n.substring(0, new_n.length-new_out.length) + new_out;
+    return new_n.substring(0, new_n.length - new_out.length) + new_out;
 
 
     function nextBigger2(n) {
         let str_n = String(n);
-        let sort_n = [... new Set(str_n.split('').map(Number).sort((x,y)=>(x-y)))].indexOf(+String(n)[0])+1;
-        return [[... new Set(str_n.split('').map(Number).sort((x,y)=>(x-y)))][sort_n], sort_n];
+        let sort_n = [... new Set(str_n.split('').map(Number).sort((x, y) => (x - y)))].indexOf(+String(n)[0]) + 1;
+        return [[... new Set(str_n.split('').map(Number).sort((x, y) => (x - y)))][sort_n], sort_n];
 
     }
 
     function getReplaseMaxMin(n) {
         console.log(n);
-        let out = String(n).split('').map(Number).sort((x,y)=>(x-y));
+        let out = String(n).split('').map(Number).sort((x, y) => (x - y));
         let numMin = out[0];
-        let numMax = out[out.length-1];
+        let numMax = out[out.length - 1];
 
         out = String(n).split('').map(Number);
 
         console.log('minmax  = ', out.join(''), numMin, numMax);
-        for (let i = 0; i < out.length; i++){
-            console.log('loop1=',out[i]);
+        for (let i = 0; i < out.length; i++) {
+            console.log('loop1=', out[i]);
             if (out[i] === numMax) {
                 out[i] = numMin;
                 break;
             }
         }
 
-        for (let i = out.length - 1; i > 0; i--){
-            console.log('loop2=',out[i]);
+        for (let i = out.length - 1; i > 0; i--) {
+            console.log('loop2=', out[i]);
             if (out[i] === numMin) {
                 out[i] = numMax;
                 break;
@@ -56,7 +56,7 @@ function nextBigger(n){
     }
 }
 
-//console.log(nextBigger(20188));
+console.log(nextBigger(20188));
 //console.log(nextBigger(201720111));
 console.log(nextBigger(59884848459853));
 console.log(59884848483559);
