5ad7b8ba809f4a11c77a2680bddb2c617167f130
basfroman <basfroman@gmail.com>
2019-04-01 23:39:33 -0700

    numbers

--- How many numbers III?.js
+++ How many numbers III?.js
@@ -4,13 +4,15 @@ function findAll(n, k) {
 
     let start = Array(k).fill(1).map((el, ind) =>
         (10 * ind < n ? n - (9 * ind) - (k - ind) > 10 ? 9 : n - (9 * ind) - (k - ind - 1) : 1)).reverse().join('');
+
     let end = String((n - (n % k)) / k).repeat(k - (n % k)) + String((n - (n % k)) / k + 1).repeat((n % k));
+
     let out = [0, start, end];
     let count = start;
     let begin = [];
     let begin_str = '';
 
-    for (let i = +start; i <= +end; i+=9) {
+    for (let i = +start; i <= +end; i += 9) {
 
         begin_str = String(i);
         begin = begin_str.split('');
@@ -25,8 +27,9 @@ function findAll(n, k) {
     return out;
 }
 
-//console.log(findAll(35, 6)); // [123, '116999', '566666'])
+console.log(findAll(35, 6)); // [123, '116999', '566666'])
 //findAll(10, 3)
 console.log(findAll(10, 3)); // [8, '118', '334']);
-//console.log(findAll(27, 3)); // [1, '999', '999']);
-//console.log(findAll(84, 4)); // []);
+console.log(findAll(27, 3)); // [1, '999', '999']);
+console.log(findAll(84, 4)); // []);
+
